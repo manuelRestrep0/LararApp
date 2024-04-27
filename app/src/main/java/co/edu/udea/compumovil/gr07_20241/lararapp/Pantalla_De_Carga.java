087fd73a3afc49e1009b0647107939e5a31e0a0a
Juan Manuel Restrepo Galarcio <94997816+manuelRestrep0@users.noreply.github.com>
2024-04-26 23:59:11 -0500

    fix:verificar usuario con sesion iniciada

--- app/src/main/java/co/edu/udea/compumovil/gr07_20241/lararapp/Pantalla_De_Carga.java
+++ app/src/main/java/co/edu/udea/compumovil/gr07_20241/lararapp/Pantalla_De_Carga.java
@@ -6,21 +6,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Pantalla_De_Carga extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_de_carga);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         int tiempo = 3000;
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(Pantalla_De_Carga.this,MainActivity.class));
+                /*startActivity(new Intent(Pantalla_De_Carga.this,MainActivity.class));
                 finish();
+                 */
+                verificarUsuario();
             }
         },tiempo);
+    }
+
+    private void verificarUsuario(){
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+        if(firebaseUser == null){
+            startActivity(new Intent(Pantalla_De_Carga.this,MainActivity.class));
+            finish();
+        }
+        else {
+            startActivity(new Intent(Pantalla_De_Carga.this,MenuPrincipal.class));
+            finish();
+        }
     }
 }
