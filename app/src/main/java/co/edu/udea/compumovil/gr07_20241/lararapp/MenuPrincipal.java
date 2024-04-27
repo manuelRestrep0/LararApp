5f38de061f01521b05fafc99d60063299cadd7c6
Juan Manuel Restrepo Galarcio <94997816+manuelRestrep0@users.noreply.github.com>
2024-04-27 00:06:35 -0500

    feature: se agrego boton para cerrar sesion

--- app/src/main/java/co/edu/udea/compumovil/gr07_20241/lararapp/MenuPrincipal.java
+++ app/src/main/java/co/edu/udea/compumovil/gr07_20241/lararapp/MenuPrincipal.java
@@ -2,13 +2,41 @@ package co.edu.udea.compumovil.gr07_20241.lararapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MenuPrincipal extends AppCompatActivity {
+
+    Button cerrarSesion;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        cerrarSesion = findViewById(R.id.CerrarSesion);
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
+        cerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                salirAplicacion();
+            }
+        });
+    }
+
+    private void salirAplicacion() {
+        firebaseAuth.signOut();
+        startActivity(new Intent(MenuPrincipal.this, MainActivity.class));
+        Toast.makeText(this,"Cerraste sesion existosamente.",Toast.LENGTH_SHORT).show();
     }
 }
