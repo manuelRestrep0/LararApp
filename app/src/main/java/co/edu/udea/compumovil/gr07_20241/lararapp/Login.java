99f7bd3ab4998ae7884c8c737707acfe150a8294
Juan Manuel Restrepo Galarcio <94997816+manuelRestrep0@users.noreply.github.com>
2024-05-07 20:28:51 -0500

    feature: funcionalidad del login

--- app/src/main/java/co/edu/udea/compumovil/gr07_20241/lararapp/Login.java
+++ app/src/main/java/co/edu/udea/compumovil/gr07_20241/lararapp/Login.java
@@ -1,14 +1,112 @@
 package co.edu.udea.compumovil.gr07_20241.lararapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+
+    EditText correoLogin, passLogin;
+    Button btnLogeo;
+    TextView usuarioNuevoTXT;
+
+    ProgressDialog progressDialog;
+
+    FirebaseAuth firebaseAuth;
+
+    String correo = "", password = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        correoLogin = findViewById(R.id.CorreoLogin);
+        passLogin = findViewById(R.id.PassPLogin);
+        btnLogeo = findViewById(R.id.Btn_Logeo);
+        usuarioNuevoTXT = findViewById(R.id.UsuarioNuevoTXT);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(Login.this);
+        progressDialog.setTitle("Espere por favor");
+        progressDialog.setCanceledOnTouchOutside(false);
+        
+        btnLogeo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ValidarDatos();
+            }
+        });
+        usuarioNuevoTXT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, Register.class));
+            }
+        });
+        
+    }
+
+    private void ValidarDatos() {
+
+        correo = correoLogin.getText().toString();
+        password = passLogin.getText().toString();
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(correo).matches()){
+            Toast.makeText(this, "Correo invalido", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Ingrese la clave", Toast.LENGTH_SHORT).show();
+        } else {
+            LoginDeUsuario();
+        }
+    }
+
+    private void LoginDeUsuario() {
+        progressDialog.setMessage("Iniciando sesion...");
+        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(correo,password)
+                .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            progressDialog.dismiss();
+                            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                            startActivity(new Intent(Login.this, MenuPrincipal.class));
+                            Toast.makeText(Login.this, "Welcome: "+firebaseUser.getEmail(), Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            progressDialog.dismiss();
+                            Toast.makeText(Login.this, "Correo o clave incorrectos", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Login.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
